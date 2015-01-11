8bc5f30294aab89a466fb84ab5064ea09d8b3a40
Snuzzo <jrracinfan@gmail.com>
2015-01-11 13:25:38 -0500

    Inherit telephony.mk

--- plain.mk
+++ plain.mk
@@ -1,5 +1,6 @@
 $(call inherit-product, vendor/plain/config/common.mk)
 $(call inherit-product, vendor/plain/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/plain/config/telephony.mk)
 $(call inherit-product, device/lge/d802/d802.mk)
 
 ## Device identifier. This must come after all inclusions
